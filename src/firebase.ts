6cfa807554575392b91e111253ce93b33e661af3
Sanha <heftyCornerstone@gmail.com>
2024-07-25 12:57:49 +0900

    storage

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyApL3RA5M9StEXhvApIw4zVmTia-fgcJYQ",
@@ -10,7 +12,10 @@ const firebaseConfig = {
   appId: "1:212040557376:web:a109db4cc6dc110563d0a5"
 };
 
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
